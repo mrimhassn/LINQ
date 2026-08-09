43a56fa221bf4e4fcf7c67100ad5baef994dcd1f
mrimhassn <mrimhassn18@gmail.com>
2026-08-09 03:18:53 +0300

    Add files via upload

---
+++ Data/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace linq1;
+
+public class Student
+{
+    public string? Name { get; set; }
+    public int Grade { get; set; }
+    public string? Major { get; set; }
+
+
+    public override string ToString()
+    => $"Name = {Name}, Grade = {Grade} And Major = {Major}";
+}
